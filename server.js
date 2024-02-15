cec8cb770cd4d6ff1ef21eaed74032f1b4754e57
jotafagundezz06 <jotafagundezz@gmail.com>
2024-02-15 14:43:42 -0300

    Foi criado uma rota, consegui criar um arquivio json localmente

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 // Importa o módulo 'mongoose' para trabalhar com o MongoDB
 const mongoose = require('mongoose');
 
+const routes = require("./routes/toDoRoute")
+
 // Importa o módulo 'dotenv' para carregar variáveis de ambiente a partir de um arquivo '.env'
 require('dotenv').config();
 
@@ -13,8 +15,12 @@ const app = express();
 // Define a porta do servidor a partir de uma variável de ambiente, ou utiliza a porta 3000 como padrão
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URl).then(() => console.log(`Connected to Mongodb...`))
-.catch((err) => console.log(err))
+// Conecta ao banco de dados MongoDB usando a URL fornecida no arquivo de variáveis de ambiente (.env)
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log(`Connected to MongoDB...`))
+  .catch((err) => console.log(err));
+
+  app.use(routes)
 
 // Inicia o servidor Express para escutar conexões na porta especificada
 app.listen(PORT, () => console.log(`Connected in: ${PORT}`));
